4a04dc01d56cbdac004d05b9a94151684843ffad
Jeya Seelan <45196752+rootxjs@users.noreply.github.com>
2025-08-23 22:29:48 +0530

    Fresh setup (#3)
    * deleted
    
    * remove older blog
    
    * init astro pure
    
    * init 1.0
    
    * site: 'https://rootxjs.github.io',
    added
    
    * fix static output in deployment

--- astro.config.ts
+++ astro.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  // adapter: github(),
   output: 'static',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
