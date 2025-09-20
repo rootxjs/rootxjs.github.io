3059efff9203ed06089d1a66bca8082bccbd9264
Jeya Seelan <45196752+rootxjs@users.noreply.github.com>
2025-09-20 03:51:28 +0000

    fixed comments and view and other import issues

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import vercel from '@astrojs/vercel'
+import sitemap from '@astrojs/sitemap'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
